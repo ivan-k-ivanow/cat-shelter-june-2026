0c5df712ec75fc5648dcd146ea56299bac7e8eab
ivan-k-ivanow <i.ivanow.k@gmail.com>
2026-06-20 15:34:38 +0300

    Add https server

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello, Cat Shelter!');
+import http from 'http';
+
+
+const server = http.createServer((req, res) => {
+    res.write('hello, world!\n');
+
+    res.end();
+});
+
+server.listen(5000, () => console.log('Server is listening on http://localhost:5000...'));
+
