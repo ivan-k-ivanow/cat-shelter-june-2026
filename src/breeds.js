34581ea278cfe93f4951bd4d4ae46f1b3c26a58e
ivan-k-ivanow <i.ivanow.k@gmail.com>
2026-06-20 18:06:24 +0300

    Add breed dynamically

---
+++ src/breeds.js
@@ -0,0 +1,10 @@
+const breeds = [
+    { id: 1, name: 'Bombay Cat'},
+    { id: 2, name: 'Siamese Cat'},
+    { id: 3, name: 'Persian Cat'},
+    { id: 4, name: 'Maine Coon'},
+    { id: 5, name: 'Bengal Cat' },
+    { id: 6, name: 'Ragdoll Cat' }
+];
+
+export default breeds;
